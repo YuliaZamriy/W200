3a8ae2b85559c27efd725ea1b19c9849097b5059
Yulia Zamriy <yulia.zamriy@gmail.com>
2018-04-18 21:20:58 -0400

    without for loop

--- text_analyzer1.py
+++ text_analyzer1.py
@@ -2,16 +2,30 @@ user_string = input("Enter text string: ")
 vowels = "aeiou"
 
 vowel_count = 0
-vowel_index = []
+first_vowel = len(user_string) + 1
 
-for l in user_string.lower():
-	if l in vowels:
-		vowel_index.append(user_string.index(l))
-		vowel_count += 1
+if "a" in user_string.lower():
+	first_vowel = min(first_vowel,user_string.lower().find("a"))
+	vowel_count += user_string.lower().count("a")
+if "e" in user_string.lower():
+	first_vowel = min(first_vowel,user_string.lower().find("e"))
+	vowel_count += user_string.lower().count("e")
+if "i" in user_string.lower():
+	first_vowel = min(first_vowel,user_string.lower().find("i"))
+	vowel_count += user_string.lower().count("i")
+if "o" in user_string.lower():
+	first_vowel = min(first_vowel,user_string.lower().find("o"))
+	vowel_count += user_string.lower().count("o")
+if "u" in user_string.lower():
+	first_vowel = min(first_vowel,user_string.lower().find("u"))
+	vowel_count += user_string.lower().count("u")
 
-if not vowel_index:
+if vowel_count == 0:
 	print("No vowels")
-else: 
+elif first_vowel < len(user_string) - 1: 
 	print("vowels:", vowel_count)
-	print("first vowel:", user_string[vowel_index[0]])
-	print("character immediately after first vowel: ", user_string[vowel_index[0]+1])
+	print("first vowel:", user_string[first_vowel])
+	print("character immediately after first vowel: ", user_string[first_vowel+1])
+else:
+	print("this vowel is the last letter of the string:", user_string[first_vowel])
+
