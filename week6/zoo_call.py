2297e45fbda016dacc005325f106301f53b17430
Yulia Zamriy <yulia.zamriy@gmail.com>
2018-05-13 17:18:13 -0400

    assignment from the book

---
+++ week6/zoo_call.py
@@ -0,0 +1,13 @@
+# from the interactice interpreter
+
+import zoo
+zoo.hours()
+
+import zoo as menagerie
+menagerie.hours()
+
+from zoo import hours
+hours()
+
+from zoo import hours as info
+info()
